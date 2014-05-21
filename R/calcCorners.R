63842665bfd8f7a3c25b963be58b458f48d90b65
unknown <Grunwals@pcrzp871.AD.Uni-Marburg.DE>
2014-05-21 12:06:22 +0200

    calccorners von ko tree burgwald 1 versuch

--- R/calcCorners.R
+++ R/calcCorners.R
@@ -1,4 +1,30 @@
-calcCorners <- function(x.known, y.known, 
+calcCorners <- function(x.known, y.known,
                         orientation, declination,
                         size.x, size.y) {
   
+  orientation <- (orientation-declination)*pi/180
+  
+  x <- sin(orientation)*size.x
+  y <- cos(orientation)*size.x
+  
+  Cx=round(x,2)+x.known
+  Cy=round(y,2)+y.known
+  
+  werteC <-data.frame(Cx,Cy)
+  
+  
+  Ax=round(x.known-cos(orientation)*size.y)
+  Ay=round(y.known+sin(orientation)*size.y)
+  
+  werteA <- data.frame(Ax,Ay)
+  
+  Bx=round(Ax+sin(orientation)*size.x,2)
+  By=round(Ay+ cos(orientation)*size.x,2)
+  
+  
+  werteB <-data.frame(Bx, By)
+  
+  werte <- data.frame(werteA, werteB, werteC, x.known, y.known)
+  werte}
+
+calcCorners(481054,5645540,9,1.6,30,48)
